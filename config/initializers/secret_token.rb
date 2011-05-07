691c2d88506838f2675be45b67df3400fce2932a
bethlingard <beth.lingard@yahoo.com>
2011-05-06 17:26:57 -0700

    created books mvc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookswapApp::Application.config.secret_token = '40543030104c99df958391a7c3160e05af587cfc8d1084d21ce5570bf070f8b106bc21eae87729fad91d6850321ec1245fc95e4884ef3371c1f856a36b938e23'
